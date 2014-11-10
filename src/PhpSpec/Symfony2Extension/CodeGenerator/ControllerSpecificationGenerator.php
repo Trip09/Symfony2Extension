db56c5269d5915c68600dbad7ba5da30adb0571a
Sergio Moya <smoya89@gmail.com>
2014-11-10 10:33:18 +0000

    Added dependencies by default

--- src/PhpSpec/Symfony2Extension/CodeGenerator/ControllerSpecificationGenerator.php
+++ src/PhpSpec/Symfony2Extension/CodeGenerator/ControllerSpecificationGenerator.php
@@ -5,6 +5,7 @@ namespace PhpSpec\Symfony2Extension\CodeGenerator;
 use PhpSpec\CodeGenerator\Generator\SpecificationGenerator;
 use PhpSpec\Locator\ResourceInterface;
 use PhpSpec\Symfony2Extension\Locator\ControllerResource;
+use Prophecy\Argument;
 
 class ControllerSpecificationGenerator extends SpecificationGenerator
 {
@@ -41,10 +42,37 @@ __halt_compiler();<?php
 namespace %namespace%;
 
 use PhpSpec\Symfony2Extension\Specification\ControllerBehavior;
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use Prophecy\Argument;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Routing\Router;
 
+/**
+ * @mixin \%subject%
+ */
 class %name% extends ControllerBehavior
 {
+    function let(Registry $doctrine, SecurityContext $securityContext, EntityRepository $repository,
+                 TokenInterface $token, UserInterface $user, EntityManager $entityManager, Request $request,
+                 FormFactory $formFactory, FormBuilder $formBuilder, Form $form, FormView $formView,
+                 Router $router, TwigEngine $templating, Response $response)
+    {
+        $this->letController(
+            $doctrine, $securityContext, $repository, $token, $user, $entityManager, $request,
+            $formFactory, $formBuilder, $form, $formView, $router, $templating, $response
+        );
+    }
+
     function it_is_container_aware()
     {
         $this->shouldHaveType('Symfony\Component\DependencyInjection\ContainerAwareInterface');
--- src/PhpSpec/Symfony2Extension/Specification/ControllerBehavior.php
+++ src/PhpSpec/Symfony2Extension/Specification/ControllerBehavior.php
@@ -2,12 +2,57 @@
 
 namespace PhpSpec\Symfony2Extension\Specification;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
 use PhpSpec\ObjectBehavior;
 use PhpSpec\Wrapper\Subject;
+use Prophecy\Argument;
+use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormFactory;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Router;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ControllerBehavior extends ObjectBehavior
 {
+    function letController(Registry $doctrine, SecurityContext $securityContext, EntityRepository $repository,
+                           TokenInterface $token, UserInterface $user, EntityManager $entityManager, Request $request,
+                           FormFactory $formFactory, FormBuilder $formBuilder, Form $form, FormView $formView,
+                           Router $router, TwigEngine $templating, Response $response)
+    {
+        $router->generate(Argument::cetera())->willReturn('test_url');
+
+        $formFactory->createBuilder(Argument::cetera())->willReturn($formBuilder);
+        $formBuilder->getForm(Argument::cetera())->willReturn($form);
+        $formFactory->create(Argument::cetera())->willReturn($form);
+        $form->createView()->willReturn($formView);
+
+        $doctrine->getManager()->willReturn($entityManager);
+        $doctrine->getRepository(Argument::any())->willReturn($repository);
+        $entityManager->getRepository(Argument::any())->willReturn($repository);
+
+        $token->getUser()->willReturn($user);
+        $securityContext->getToken()->willReturn($token);
+
+        $templating->renderResponse(Argument::cetera())->willReturn($response);
+
+        $this->container->get('security.context')->willReturn($securityContext);
+        $this->container->has('doctrine')->willReturn(true);
+        $this->container->get('doctrine')->willReturn($doctrine);
+        $this->container->get('form.factory')->willReturn($formFactory);
+        $this->container->get('request')->willReturn($request);
+        $this->container->get('router')->willReturn($router);
+        $this->container->get('templating')->willReturn($templating);
+    }
+
     /**
      * @var ContainerInterface|null
      */
